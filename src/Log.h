dff8321de0e1c3f7303b5725818a1bcc9c2eabbb
Jonathan Sönnerup <nooxet@gmail.com>
2022-04-21 17:06:50 +0200

    Added more logging levels

--- src/Log.h
+++ src/Log.h
@@ -19,3 +19,5 @@ namespace NyetFlix
 #define NF_CORE_TRACE(...)	::NyetFlix::Log::getCoreLogger()->trace(__VA_ARGS__)
 #define NF_CORE_INFO(...)	::NyetFlix::Log::getCoreLogger()->info(__VA_ARGS__)
 #define NF_CORE_WARN(...)	::NyetFlix::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define NF_CORE_ERROR(...)	::NyetFlix::Log::getCoreLogger()->error(__VA_ARGS__)
+#define NF_CORE_FATAL(...)	::NyetFlix::Log::getCoreLogger()->fatal(__VA_ARGS__)
